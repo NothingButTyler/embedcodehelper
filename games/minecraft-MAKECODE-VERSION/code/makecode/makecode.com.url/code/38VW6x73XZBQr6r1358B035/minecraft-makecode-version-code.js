c9fcfe2977ba4ccf35c5350fb567aca1b96ff636
NothingButTyler <149272952+NothingButTyler@users.noreply.github.com>
2024-01-03 19:10:41 -0700

    Create minecraft-makecode-version-code.js

---
+++ games/minecraft-MAKECODE-VERSION/code/makecode/makecode.com.url/code/38VW6x73XZBQr6r1358B035/minecraft-makecode-version-code.js
@@ -0,0 +1,4 @@
+  makecode:
+    home_url: https://minecraft.makecode.com/
+
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.embedcodehelper }}", "{{ site.github.NothingButTyler }}/{{ site.github.embedcodehelper }}");<
